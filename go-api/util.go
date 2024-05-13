3a0d4535ecb6f3d02a78f994dae732eea144cba3
TecLegacy <kshvkumar.kk2@gmail.com>
2024-05-13 07:39:25 +0530

    refactored util files

--- go-api/util.go
+++ go-api/util.go
@@ -1,6 +1,13 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
 
 func ScanIntoAccountFromRow(row *sql.Row) (*Account, error) {
 	account := new(Account)
@@ -12,4 +19,16 @@ func ScanIntoAccountFromRows(rows *sql.Rows) (*Account, error) {
 	account := new(Account)
 	err := rows.Scan(&account.ID, &account.FirstName, &account.LastName, &account.AccNumber, &account.Balance, &account.CreatedAt)
 	return account, err
+}
+
+func getID(r *http.Request) (int, error) {
+	idStr := mux.Vars(r)["id"]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return id, fmt.Errorf("invalid id provided. %s", idStr)
+	}
+
+	return id, nil
+
 }
