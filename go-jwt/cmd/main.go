5adedf6e7eb1830320cdd107928c8d3ad57e409e
TecLegacy <kshvkumar.kk2@gmail.com>
2024-06-09 03:11:14 +0530

    Docker mysql image & go databse connection setup

--- go-jwt/cmd/main.go
+++ go-jwt/cmd/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/teclegacy/golang-ecom/cmd/api"
+	"github.com/teclegacy/golang-ecom/config"
+	db_ "github.com/teclegacy/golang-ecom/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+
+	db, err := db_.NewMySqlStorage(mysql.Config{
+		User:   config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Addr:   config.Envs.DBAddress,
+		DBName: config.Envs.DBName,
+
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Establish connection with MYSQL DB
+	// Ping it
+	db_.InitStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
