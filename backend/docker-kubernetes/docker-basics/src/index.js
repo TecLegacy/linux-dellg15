fb489729f9dc74a8afb6d41fc66691af737d4f30
TecLegacy <kshvkumar.kk2@gmail.com>
2024-07-08 16:13:16 +0530

    Image previewer Node & docker

--- backend/docker-kubernetes/docker-basics/src/index.js
+++ backend/docker-kubernetes/docker-basics/src/index.js
@@ -3,8 +3,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.render('index', { title: 'Hello World!' });
+});
+
+app.post('/api/v1/echo', (req, res) => {
+  const { name, imgUrl, message } = req.body;
+  res.status(201).json({ message });
 });
 
 app.listen(port, () => {
