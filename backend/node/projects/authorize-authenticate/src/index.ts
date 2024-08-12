c00e73a958dcf7c08afa5354f872c3eac3d3b691
TecLegacy <kshvkumar.kk2@gmail.com>
2024-08-12 10:54:03 +0530

    Setting up prettierrc

---
+++ backend/node/projects/authorize-authenticate/src/index.ts
@@ -0,0 +1,13 @@
+const firstname: string = 'John'
+const lastname: string = 'Doe'
+const age: number = 30
+
+const sayHello = (name: string) => {
+    console.log(`Hello ${name}`)
+}
+sayHello('John Doe')
+
+console.log(firstname, lastname, age)
+
+const obj = { name: 'John Doe', age: 30 }
+console.log(obj)
