04dfa14040f85f46f08498e31251d7f76ab5d65f
TecLegacy <kshvkumar.kk2@gmail.com>
2024-08-19 21:55:29 +0530

    refactor validation error in isAuth middleware

--- backend/node/projects/authorize-authenticate/src/middleware/validate-user.ts
+++ backend/node/projects/authorize-authenticate/src/middleware/validate-user.ts
@@ -1,16 +1,15 @@
 import { RequestValidationError } from '@errors/RequestValidationError'
 import { type NextFunction, Response, Request } from 'express'
 import { validationResult } from 'express-validator'
+import asyncHandler from 'express-async-handler'
 
-export const isValidUser = (
-    req: Request,
-    _: Response,
-    next: NextFunction
-): void => {
-    const errors = validationResult(req)
+export const isValidUser = asyncHandler(
+    (req: Request, _: Response, next: NextFunction): void => {
+        const errors = validationResult(req)
 
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array())
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array())
+        }
+        next()
     }
-    next()
-}
+)
