1bbba41f1777c31a8b75ff1f5abdc4bb6728c8c3
keshavAu <Keshav@blity.com.au>
2025-05-02 18:12:34 +0530

    tailwindcss setup v4

---
+++ frontend/edroh/rental-next-aws/tailwind-test/client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className=' bg-red-200'>hello</div>;
+      <div className=' bg-primary-700  text-white'>yolo</div>;
+      <div className=' bg-secondary-950  text-white'>yolo222</div>;
+    </>
+  );
+}
