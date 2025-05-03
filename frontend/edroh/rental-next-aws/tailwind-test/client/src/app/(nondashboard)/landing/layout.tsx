524ba4a7cc6f338b61060ae7db69fd9ee81e502d
keshavAu <Keshav@blity.com.au>
2025-05-04 02:45:41 +0530

    navbar

---
+++ frontend/edroh/rental-next-aws/tailwind-test/client/src/app/(nondashboard)/landing/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from '@/components/Navbar';
+import { NAVBAR_HEIGHT } from '@/lib/constants';
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+
+      <main className={`h-full w-full flex flex-col pt-[${NAVBAR_HEIGHT}px]`}>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
